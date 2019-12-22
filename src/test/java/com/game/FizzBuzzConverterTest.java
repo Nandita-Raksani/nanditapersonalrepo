c6615f00e71a393cdc6a20f3d2b67508487d0c87
Nandita Raksani <nrak15@gmail.com>
2019-12-22 10:15:02 +0530

    Added test case to check if the number is converted as 98 when the given number is 98

--- src/test/java/com/game/FizzBuzzConverterTest.java
+++ src/test/java/com/game/FizzBuzzConverterTest.java
@@ -9,5 +9,6 @@ public class FizzBuzzConverterTest {
         FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
         Assert.assertEquals(1,fizzBuzzConverter.convert(1));
         Assert.assertEquals(2,fizzBuzzConverter.convert(2));
+        Assert.assertEquals(98,fizzBuzzConverter.convert(98));
     }
 }
