65b5767363da67c039712747cf85b44d140421e9
Nandita Raksani <nrak15@gmail.com>
2019-12-22 11:20:45 +0530

    Moved the class initialization common to be reused by all methods.

--- src/test/java/com/game/FizzBuzzConverterTest.java
+++ src/test/java/com/game/FizzBuzzConverterTest.java
@@ -4,19 +4,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class FizzBuzzConverterTest {
+    FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
+
     @Test
-    public void converterReturnsNormalNumberAsIs(){
-        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-        Assert.assertEquals("1",fizzBuzzConverter.convert(1));
-        Assert.assertEquals("2",fizzBuzzConverter.convert(2));
-        Assert.assertEquals("98",fizzBuzzConverter.convert(98));
+    public void converterReturnsNormalNumberAsIs() {
+        Assert.assertEquals("1", fizzBuzzConverter.convert(1));
+        Assert.assertEquals("2", fizzBuzzConverter.convert(2));
+        Assert.assertEquals("98", fizzBuzzConverter.convert(98));
     }
 
     @Test
-    public void converterReturnsFizzForMultiplesOfThree(){
-        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-        Assert.assertEquals("Fizz",fizzBuzzConverter.convert(3));
-        Assert.assertEquals("Fizz",fizzBuzzConverter.convert(6));
-        Assert.assertEquals("Fizz",fizzBuzzConverter.convert(99));
+    public void converterReturnsFizzForMultiplesOfThree() {
+        Assert.assertEquals("Fizz", fizzBuzzConverter.convert(3));
+        Assert.assertEquals("Fizz", fizzBuzzConverter.convert(6));
+        Assert.assertEquals("Fizz", fizzBuzzConverter.convert(99));
     }
 }
